b417761ccc9a30a4b5cb25eeda66ecbe7a4365fd
Walther0296 <sau100@inbox.ru>
2023-08-31 20:21:25 +0300

    Выполненная программа №1

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Урок 7. Двумерные массивы
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+System.Console.WriteLine("Задача №1.");
+
+int m = Prompt("Количество столбцов массива ");
+int n = Prompt("Количество строк массива ");
+float[,] array = GenerateArray(m, n);
+PrintArray(array);
+
+// Генерируем рандомный массив, задаем параметры массива
+float [,] GenerateArray(int m, int n)
+{
+    float[,] array = new float[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 9);
+        }
+    }
+    return array;
+}
+
+// Выводим полученный массив на экран
+void PrintArray(float [,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine("");
+    }
+}
+
+
+
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
