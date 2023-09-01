1bce5896ba57b17eb81c30972dd7384911fad34a
Walther0296 <sau100@inbox.ru>
2023-09-01 10:27:42 +0300

    Выполнение задачи №3

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -22,21 +22,21 @@ double[,] array = GenerateArray(m, n);
 PrintArray(array);
 
 // Генерируем рандомный массив, задаем параметры массива
-double [,] GenerateArray(int m, int n)
+double[,] GenerateArray(int m, int n)
 {
     double[,] array = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = new Random().NextDouble()*10;
+            array[i, j] = new Random().NextDouble() * 10;
         }
     }
     return array;
 }
 
 // Выводим полученный массив на экран
-void PrintArray(double [,] array)
+void PrintArray(double[,] array)
 {
     for (int i = 0; i < m; i++)
     {
@@ -59,11 +59,11 @@ void PrintArray(double [,] array)
 System.Console.WriteLine("Задача №2. Вывод конкретного элемента массива.");
 int row = Prompt("Введите номер строки: ");
 int line = Prompt("Введите номер столбца: ");
-if ((n<line) || (m<row)) {
+if ((n < line) || (m < row))
+{
     System.Console.WriteLine("Элемент массива с указанными координатами отсутствует");
 }
-System.Console.Write("{0,6:F2}",array[row-1, line-1]);
-
+System.Console.WriteLine("{0,6:F2}", array[row - 1, line - 1]);
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
@@ -74,4 +74,52 @@ System.Console.Write("{0,6:F2}",array[row-1, line-1]);
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 // Генерируем рандомный массив, задаем параметры массива
 
+System.Console.WriteLine("Задача №3. Вывод average каждого столбца нового целочисленного массива");
 
+int m1 = Prompt("Количество строк целочисленного массива ");
+int n1 = Prompt("Количество столбцов целочисленного массива ");
+int[,] array1 = GenerateArrayInt(m1, n1);
+PrintArrayInt(array1);
+CountAverage(array1);
+
+int[,] GenerateArrayInt(int m1, int n1)
+{
+    int[,] array1 = new int[m1, n1];
+    Random random = new Random();
+    for (int i = 0; i < m1; i++)
+    {
+        for (int j = 0; j < n1; j++)
+        {
+            array1[i, j] = random.Next(1, 9);
+        }
+    }
+    return array1;
+}
+void PrintArrayInt(int[,] array1)
+{
+    for (int i = 0; i < m1; i++)
+    {
+        for (int j = 0; j < n1; j++)
+        {
+            System.Console.Write(array1[i, j] + " ");
+        }
+        System.Console.WriteLine("");
+    }
+}
+
+void CountAverage(int[,] array1)
+{
+    for (int i = 0; i < n1; i++)
+    {
+        int sum = 0;
+        double average = 0;
+        for (int j = 0; j < m1; j++)
+        {
+            sum += array1[j, i];
+            average = sum / n1;
+        }
+        System.Console.WriteLine(
+            $"Среднее арифметическое элементов массива столбца {i + 1}: {average}"
+        );
+    }
+}
