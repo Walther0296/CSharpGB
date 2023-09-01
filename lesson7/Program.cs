47d2affc605947612552a99b2108b849226fa9fa
Walther0296 <sau100@inbox.ru>
2023-09-01 08:33:01 +0300

    Pass Task2

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -3,7 +3,7 @@
 int Prompt(string message)
 {
     System.Console.Write(message);
-    string value = Console.ReadLine();
+    string value = System.Console.ReadLine();
     int result = Convert.ToInt32(value);
     return result;
 }
@@ -16,43 +16,37 @@ int Prompt(string message)
 // 8 7,8 -7,1 9
 System.Console.WriteLine("Задача №1.");
 
-int m = Prompt("Количество столбцов массива ");
-int n = Prompt("Количество строк массива ");
-float[,] array = GenerateArray(m, n);
+int m = Prompt("Количество строк массива ");
+int n = Prompt("Количество столбцов массива ");
+double[,] array = GenerateArray(m, n);
 PrintArray(array);
 
 // Генерируем рандомный массив, задаем параметры массива
-float [,] GenerateArray(int m, int n)
+double [,] GenerateArray(int m, int n)
 {
-    float[,] array = new float[m, n];
+    double[,] array = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = new Random().Next(0, 9);
+            array[i, j] = new Random().NextDouble()*10;
         }
     }
     return array;
 }
 
 // Выводим полученный массив на экран
-void PrintArray(float [,] array)
+void PrintArray(double [,] array)
 {
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            System.Console.Write($"{array[i, j]} ");
+            System.Console.Write("{0,6:F2}", array[i, j]);
         }
         System.Console.WriteLine("");
     }
 }
-
-
-
-
-
-
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
@@ -62,11 +56,13 @@ void PrintArray(float [,] array)
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-
-
-
-
-
+System.Console.WriteLine("Задача №2. Вывод конкретного элемента массива.");
+int row = Prompt("Введите номер строки: ");
+int line = Prompt("Введите номер столбца: ");
+if ((n<line) || (m<row)) {
+    System.Console.WriteLine("Элемент массива с указанными координатами отсутствует");
+}
+System.Console.Write("{0,6:F2}",array[row-1, line-1]);
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
@@ -76,3 +72,6 @@ void PrintArray(float [,] array)
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// Генерируем рандомный массив, задаем параметры массива
+
+
