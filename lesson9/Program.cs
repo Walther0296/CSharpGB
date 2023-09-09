e0eae39d38d3f9cd2752278dd9377b47ffc257ba
Walther0296 <sau100@inbox.ru>
2023-09-09 11:28:01 +0300

    Выполненное первое задание

---
+++ lesson9/Program.cs
@@ -0,0 +1,39 @@
+﻿// Урок 9. Рекурсия
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = System.Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine("Задача №1.");
+
+int N = Prompt("Введите число N: ");
+int Ex = 1; 
+System.Console.Write(NumbersRec(N, Ex));
+
+string NumbersRec (int N, int Ex){
+    if (N>Ex) return $"{N}, " + NumbersRec(N-1, Ex); 
+    else return $"{Ex}."; 
+}
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+
+
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
